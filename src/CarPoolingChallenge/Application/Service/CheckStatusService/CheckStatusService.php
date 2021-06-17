f6188f8965a02faa93c3015e68e3259d1eb16ab8
gonzalo <gonzalosantos89@gmail.com>
2021-06-17 14:17:58 +0200

    fix code style

--- src/CarPoolingChallenge/Application/Service/CheckStatusService/CheckStatusService.php
+++ src/CarPoolingChallenge/Application/Service/CheckStatusService/CheckStatusService.php
@@ -6,7 +6,6 @@ use Gonsandia\CarPoolingChallenge\Application\Service\ApplicationService;
 
 class CheckStatusService implements ApplicationService
 {
-
     public function execute($request = null): bool
     {
         return true;
