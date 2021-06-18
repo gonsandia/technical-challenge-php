2c421c3a00d167364f1ff334faddce6ec5f2a139
gonzalo <gonzalosantos89@gmail.com>
2021-06-18 10:30:24 +0200

    missing file

---
+++ src/CarPoolingChallenge/Domain/Model/AvailableSeats.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Gonsandia\CarPoolingChallenge\Domain\Model;
+
+use Assert\Assert;
+
+class AvailableSeats
+{
+    public const MIN_SEATS = 0;
+    public const MAX_SEATS = TotalSeats::MAX_SEATS;
+    private int $count;
+
+    /**
+     * TotalSeats constructor.
+     * @param int $count
+     */
+    public function __construct(int $count)
+    {
+        Assert::that($count)->between(self::MIN_SEATS, self::MAX_SEATS);
+
+        $this->count = $count;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCount(): int
+    {
+        return $this->count;
+    }
+
+    public function __toString()
+    {
+        return (string)$this->getCount();
+    }
+
+    public function equals($other): bool
+    {
+        return $this->getCount() === $other->getCount();
+    }
+}
